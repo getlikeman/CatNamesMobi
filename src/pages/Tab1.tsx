4af44b924850d9d39ffc3d3a0eb7058da3898fa2
getlikeman <misternikir@gmail.com>
2023-09-30 19:00:27 +0600

    1.0

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,25 +1,146 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import {IonContent, IonHeader, IonItem, IonList, IonPage, IonTitle, IonToggle, IonToolbar} from '@ionic/react';
+import React, {useEffect, useState} from "react";
+import { Storage } from '@ionic/storage';
+let data = [
+    {
+        "id": "6782",
+        "check": false,
+        "name": "Айет"
+    },
+    {
+        "id": "11071",
+        "check": false,
+        "name": "Айтеке-Би"
+
+    },
+    {
+        "id": "2728",
+        "check": false,
+        "name": "Акколь"
+
+    },
+    {
+        "id": "150",
+        "check": false,
+        "name": "Аксай (Казахстан)"
+
+    },
+    {
+        "id": "6254",
+        "check": false,
+        "name": "Аксукент"
+
+    },
+    {
+        "id": "151",
+        "check": false,
+        "name": "Аксу (Павлодар.обл)"
+
+    },
+    {
+        "id": "152",
+        "check": false,
+        "name": "Актау"
+
+    },
+    {
+        "id": "154",
+        "check": false,
+        "name": "Актобе"
+
+    },
+    {
+        "id": "6365",
+        "check": false,
+        "name": "Актогай"
+
+    },
+    {
+        "id": "156",
+        "check": false,
+        "name": "Алга (Актюбинская обл)"
+
+    },
+    {
+        "id": "160",
+        "check": false,
+        "name": "Алматы"
+
+    },
+    {
+        "id": "169",
+        "check": false,
+        "name": "Алтай (Зыряновск)"
+
+    },
+    {
+        "id": "158",
+        "check": false,
+        "name": "Аральск"
+    },
+    {
+        "id": "161",
+        "check": false,
+        "name": "Аркалык"
+    },
+    {
+        "id": "6300",
+        "check": false,
+        "name": "Арысь (Туркестанская область)"
+    }]
 
 const Tab1: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
-      </IonContent>
-    </IonPage>
-  );
+    let [items, setItems] = useState([]) as any
+    let [filter, setFilter] = useState(false)
+    const store = new Storage();
+    store.create()
+    async function dataStorage() {
+
+
+        let st= await store.get('key')
+        if(st== undefined){
+            await store.set('key', data)
+        }else{
+            return await store.get('key')
+        }
+    }
+    useEffect(() => {
+        dataStorage().then(res=>setItems(res) )
+    }, []);
+
+async function  f(index: string) {
+ // @ts-ignore
+    let newData=items.map((item:any) =>{
+        if (item.id===index){
+            return{...item,check:!item.check}
+        }else {
+            return item
+        }
+    })
+    setItems(newData)
+    await store.set('key',newData)
+}
+
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Кононец Никита РК-3</IonTitle><IonTitle><IonToggle checked={filter} onClick={()=> setFilter(!filter) }>выбранные</IonToggle></IonTitle>
+
+                </IonToolbar>
+            </IonHeader>
+            <IonContent fullscreen>
+
+                <IonList>
+                    {!filter ? items.map((item:any) => {
+                        return <IonItem key={item.id}><IonToggle checked={item.check} onClick={()=> f(item.id)} >{item.name}</IonToggle></IonItem>
+                    }) : items.filter((pipe:any) =>pipe.check).map((item:any) => {
+                        return <IonItem key={item.id}><IonToggle checked={item.check}  onClick={()=> f(item.id)}>{item.name}</IonToggle></IonItem>
+                    })}
+                </IonList>
+            </IonContent>
+        </IonPage>
+    );
 };
 
 export default Tab1;
--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,25 +1,62 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab2.css';
+import {IonContent, IonHeader, IonItem, IonList, IonPage, IonTitle, IonToggle, IonToolbar} from '@ionic/react';
+import { initializeApp } from "firebase/app";
+import { getDatabase,ref ,get,child,set} from "firebase/database";
+import React, {useEffect, useState} from "react";
+
+const firebaseConfig = {
+   apiKey: "AIzaSyBmxrC_C4QlJs4-JO6Ap5HSp_e98dRxCc8",
+  authDomain: "test-24dd8.firebaseapp.com",
+  databaseURL: "https://test-24dd8-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "test-24dd8",
+  storageBucket: "test-24dd8.appspot.com",
+  messagingSenderId: "599038139951",
+  appId: "1:599038139951:web:6c0c9c5c9039317304b0da",
+
+};
+
+const app = initializeApp(firebaseConfig);
+let data =getDatabase(app)
 
 const Tab2: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
-      </IonContent>
-    </IonPage>
-  );
+    let [items, setItems] = useState([]) as any
+    let [filter, setFilter] = useState(false)
+    useEffect(() => {
+      get(child(ref(data),'/')).then(res =>{
+
+          setItems(res.val().date)
+      })
+    }, []);
+
+    async function  f(index: string) {
+        // @ts-ignore
+        let newData=items.map((item:any) =>{
+            if (item.id===index){
+                return{...item,check:!item.check}
+            }else {
+                return item
+            }
+        })
+        setItems(newData)
+        set(ref(data,'/'),{date:newData})
+    }
+    return (
+        <IonPage>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Кононец Никита РК-4</IonTitle><IonTitle><IonToggle checked={filter} onClick={()=> setFilter(!filter) }>выбранные</IonToggle></IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonContent fullscreen>
+                <IonList>
+                    {!filter ? items.map((item:any) => {
+                        return <IonItem key={item.id}><IonToggle checked={item.check} onClick={()=> f(item.id)} >{item.name}</IonToggle></IonItem>
+                    }) : items.filter((pipe:any) =>pipe.check).map((item:any) => {
+                        return <IonItem key={item.id}><IonToggle checked={item.check}  onClick={()=> f(item.id)}>{item.name}</IonToggle></IonItem>
+                    })}
+                </IonList>
+            </IonContent>
+        </IonPage>
+    );
 };
 
 export default Tab2;
